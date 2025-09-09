b42d4d5ba0c9237ccde105eb89c1cc44228d5fdd
pgrinsell <phillip.grinsell3@gmail.com>
2025-09-09 15:28:30 -0400

    node types defined

---
+++ src/htmlnode.py
@@ -0,0 +1,50 @@
+import html
+
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children if children is not None else []
+        self.props = props if props is not None else {}
+
+    def props_to_html(self):
+        if not self.props:
+            return ""
+        return " " + " ".join(f'{key}="{html.escape(str(value), quote=True)}"' for key, value in self.props.items())
+
+
+    def __repr__(self):
+        return (
+            f"HTMLNode(tag={self.tag!r}, value={self.value!r}, "
+            f"children={self.children!r}, props={self.props!r})"
+        )
+
+class LeafNode(HTMLNode):
+    def __init__(self, value, tag=None, props=None):
+        super().__init__(tag=tag, value=value, children=[], props=props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNode must have a value")
+        props_str = self.props_to_html()
+        escaped_value = html.escape(str(self.value))
+        if self.tag:
+            return f"<{self.tag}{props_str}>{escaped_value}</{self.tag}>"
+        return escaped_value
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        if not tag:
+            raise ValueError("ParentNode must have a tag")
+        if children is None or not isinstance(children, list) or len(children) == 0:
+            raise ValueError("ParentNode must have children")
+        for child in children:
+            if not isinstance(child, HTMLNode):
+                raise ValueError("Children must be instances of HTMLNode")
+        super().__init__(tag=tag, value=None, children=children, props=props)
+
+    def to_html(self):
+        props_str = self.props_to_html()
+        children_html = "".join(child.to_html() for child in self.children)
+        return f"<{self.tag}{props_str}>{children_html}</{self.tag}>"
+
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 class TextType(Enum):
     TEXT = "text"
     BOLD = "bold"
@@ -22,3 +23,28 @@ class TextNode:
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(text_node.text, None)
+
+    elif text_node.text_type == TextType.BOLD:
+        return LeafNode(text_node.text, "b")
+
+    elif text_node.text_type == TextType.ITALIC:
+        return LeafNode(text_node.text, "i")
+
+    elif text_node.text_type == TextType.CODE:
+        return LeafNode(text_node.text, "code")
+
+    elif text_node.text_type == TextType.LINK:
+        if not text_node.url:
+            raise ValueError("LINK type requires a URL")
+        return LeafNode(text_node.text, "a", props={"href": text_node.url})
+
+    elif text_node.text_type == TextType.IMAGE:
+        if not text_node.url:
+            raise ValueError("IMAGE type requires a URL")
+        return LeafNode('', "img", props={"src": text_node.url, "alt": text_node.text})
+
+    else:
+        raise ValueError(f"Unsupported TextType: {text_node.text_type}")
